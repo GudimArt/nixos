7d50354ab4e02111a110b18c36d369bf7bbab170
GudimArt <gudimovartm@gmail.com>
2024-05-01 01:36:40 +0700

    fix

--- flake.nix
+++ flake.nix
@@ -28,48 +28,45 @@
             disko.devices = {
               disk = {
                 my-disk = {
-                device = "/dev/sda";
-                type = "disk";
-                content = {
-                  type = "gpt";
+                  device = "/dev/sda";
+                  type = "disk";
+                  content = {
+                    type = "gpt";
+                    partitions = {
+                      ESP = {
+                        type = "EF00";
+                        size = "500M";
+                        content = {
+                        type = "filesystem";
+                        format = "vfat";
+                        mountpoint = "/boot";
+                        };
+                      };
 
 
-                  partitions = {
-                  ESP = {
-                    type = "EF00";
-                    size = "500M";
-                    content = {
-                    type = "filesystem";
-                    format = "vfat";
-                    mountpoint = "/boot";
+                      swap = {
+                        size = "8G";
+                        content = {
+                        type="swap";
+                        resumeDevice = true;
+                        };
+                      };
+
+
+                      root = {
+                        size = "100%";
+                        content = {
+                        type = "filesystem";
+                        format = "btrfs";
+                        mountpoint = "/";
+                        };
+                      };
                     };
                   };
-
-
-                  swap = {
-                    size = "8G";
-                    content = {
-                    type="swap";
-                    resumeDevice = true;
-                    };
-                  };
-
-
-                  root = {
-                    size = "100%";
-                    content = {
-                    type = "filesystem";
-                    format = "btrfs";
-                    mountpoint = "/";
-                    };
-
-                  };
-                  };
-                };
                 };
               };
             };
-          };
+          }
         ];
       };
 
