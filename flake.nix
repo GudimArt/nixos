1c34ea6d07eb546a900f8115e580601f72a20dbb
GudimArt <gudimovartm@gmail.com>
2024-05-01 01:20:24 +0700

    test

--- flake.nix
+++ flake.nix
@@ -24,6 +24,52 @@
         modules = [
           ./configuration.nix
           disko.nixosModules.disko
+          {
+            disko.devices = {
+              disk = {
+                my-disk = {
+                device = "/dev/sda";
+                type = "disk";
+                content = {
+                  type = "gpt";
+
+
+                  partitions = {
+                  ESP = {
+                    type = "EF00";
+                    size = "500M";
+                    content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    };
+                  };
+
+
+                  swap = {
+                    size = "8G";
+                    content = {
+                    type="swap";
+                    resumeDevice = true;
+                    };
+                  };
+
+
+                  root = {
+                    size = "100%";
+                    content = {
+                    type = "filesystem";
+                    format = "btrfs";
+                    mountpoint = "/";
+                    };
+
+                  };
+                  };
+                };
+                };
+              };
+            };
+          };
         ];
       };
 
